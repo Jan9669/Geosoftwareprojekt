9b3a2411aebe078917e13cb9f81afdd59797301c
Jan9669 <82373118+Jan9669@users.noreply.github.com>
2021-09-11 17:50:28 +0200

    Add files via upload

---
+++ public/Haltestellen.js
@@ -0,0 +1,31 @@
+console.log("1642")
+
+
+
+/*
+$.getJSON("Haltestellenjson.json", function(json) {
+    anzahlhs=json.features.length
+    
+  var hskoordinatenarray=[]
+  for(let a=0; a<anzahlhs;a++){
+    
+   
+    
+  
+   hskoordinatenarray[a]=json.features[a].geometry.coordinates
+  
+   } 
+   
+   
+   console.log(hskoordinatenarray[1][1])
+});
+*/
+
+
+
+
+
+
+
+
+
